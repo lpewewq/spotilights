2a9df3a80df6f75fbbb7bd7f28c34d4ca1b8e70d
Lars Fischer <lars.fischer@rwth-aachen.de>
2021-10-19 01:15:43 +0200

    pride fix

--- app/visualizer/ambient/pride.py
+++ app/visualizer/ambient/pride.py
@@ -21,24 +21,23 @@ class PrideVisualizer(BaseVisualizer):
         super().__init__(app)
         self.sPseudotime = ct.c_uint16(0)
         self.sHue16 = ct.c_uint16(0)
-        self.fps = 60
 
     def update(self, delta):
         deltams = ct.c_uint16(int(delta * 1000))
 
-        sat8 = ct.c_uint8(int(beatsin(0.3, 220, 250)))
-        brightdepth = ct.c_uint8(int(beatsin(1.3, 96, 224)))
-        brightnessthetainc16 = ct.c_uint16(int(beatsin(0.8, 6400, 10240)))
-        msmultiplier = ct.c_uint8(int(beatsin(0.6, 23, 60)))
+        sat8 = ct.c_uint8(int(beatsin(87.0 / 256, 220, 250)))
+        brightdepth = ct.c_uint8(int(beatsin(341.0 / 256, 96, 224)))
+        brightnessthetainc16 = ct.c_uint16(int(beatsin(203.0 / 256, 6400, 10240)))
+        msmultiplier = ct.c_uint8(int(beatsin(147.0 / 256, 23, 60)))
 
         hue16 = ct.c_uint16(self.sHue16.value)
-        hueinc16 = ct.c_uint16(int(beatsin(0.4, 1, 3000)))
+        hueinc16 = ct.c_uint16(int(beatsin(113.0 / 256, 1, 3000)))
 
         self.sPseudotime = ct.c_uint16(
             self.sPseudotime.value + deltams.value * msmultiplier.value
         )
         self.sHue16 = ct.c_uint16(
-            self.sHue16.value + deltams.value * int(beatsin(1.6, 5, 9))
+            self.sHue16.value + deltams.value * int(beatsin(400.0 / 256, 5, 9))
         )
         brightnesstheta16 = ct.c_uint16(self.sPseudotime.value)
 
@@ -50,12 +49,12 @@ class PrideVisualizer(BaseVisualizer):
                 brightnesstheta16.value + brightnessthetainc16.value
             )
             b16 = ct.c_uint16(
-                int(np.sin(np.pi * (brightnesstheta16.value / 32768)) + 1 * 32768)
+                int((np.sin(np.pi * (brightnesstheta16.value / 32768)) + 1) * 32768)
             )
 
             bri16 = ct.c_uint16(int((b16.value * b16.value) / 65536))
             bri8 = ct.c_uint8(int((bri16.value * brightdepth.value) / 65536))
-            bri8 = ct.c_uint8(bri8.value + (255 - brightdepth.value))
+            bri8 = ct.c_uint8(bri8.value + 255 - brightdepth.value)
             newcolor = RGB(
                 *hsv_to_rgb(hue8.value / 255, sat8.value / 255, bri8.value / 255)
             )
@@ -63,5 +62,4 @@ class PrideVisualizer(BaseVisualizer):
             led.r = (3 * led.r + newcolor.r) / 4
             led.g = (3 * led.g + newcolor.g) / 4
             led.b = (3 * led.b + newcolor.b) / 4
-        time.sleep(1.0 / self.fps)
         return self.leds

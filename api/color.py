79af60831f541ef985a041042f3aad9c43fa9d80
lpewewq <lpewewq@gmail.com>
2022-10-07 20:35:49 +0200

    color clamping

--- api/color.py
+++ api/color.py
@@ -1,8 +1,11 @@
 from colorsys import hsv_to_rgb
 from dataclasses import dataclass
-from typing import Tuple
 
-from pydantic import BaseModel, confloat, conint
+from pydantic import BaseModel, conint
+
+
+def clamp(x):
+    return min(255, max(0, int(x)))
 
 
 @dataclass
@@ -14,9 +17,9 @@ class Color:
     @classmethod
     def from_hsv(cls, h: float, s: float, v: float) -> "Color":
         r, g, b = hsv_to_rgb(h, s, v)
-        r = int(r * 255) & 255
-        g = int(g * 255) & 255
-        b = int(b * 255) & 255
+        r = clamp(r * 255)
+        g = clamp(g * 255)
+        b = clamp(b * 255)
         return Color(r, g, b)
 
     @classmethod
@@ -32,7 +35,7 @@ class Color:
         b = int(self.b) & 255
         return (r << 16) | (g << 8) | b
 
-    def as_bytes(self) -> Tuple[int, int, int]:
+    def as_bytes(self) -> tuple[int, int, int]:
         r = int(self.r) & 255
         g = int(self.g) & 255
         b = int(self.b) & 255
@@ -40,31 +43,31 @@ class Color:
 
     def blend(self, other: "Color", percentage: float = 0.5) -> "Color":
         keep = 1 - percentage
-        r = int(self.r * keep + other.r * percentage) & 255
-        g = int(self.g * keep + other.g * percentage) & 255
-        b = int(self.b * keep + other.b * percentage) & 255
+        r = clamp(self.r * keep + other.r * percentage)
+        g = clamp(self.g * keep + other.g * percentage)
+        b = clamp(self.b * keep + other.b * percentage)
         return Color(r, g, b)
 
     def __add__(self, other):
         if isinstance(other, self.__class__):
-            r = int(self.r + other.r) & 255
-            g = int(self.g + other.g) & 255
-            b = int(self.b + other.b) & 255
+            r = clamp(self.r + other.r)
+            g = clamp(self.g + other.g)
+            b = clamp(self.b + other.b)
         else:
-            r = int(self.r + other) & 255
-            g = int(self.g + other) & 255
-            b = int(self.b + other) & 255
+            r = clamp(self.r + other)
+            g = clamp(self.g + other)
+            b = clamp(self.b + other)
         return Color(r, g, b)
 
     def __mul__(self, other):
         if isinstance(other, self.__class__):
-            r = int(self.r * other.r / 255) & 255
-            g = int(self.g * other.g / 255) & 255
-            b = int(self.b * other.b / 255) & 255
+            r = clamp(self.r * other.r / 255)
+            g = clamp(self.g * other.g / 255)
+            b = clamp(self.b * other.b / 255)
         else:
-            r = int(self.r * other) & 255
-            g = int(self.g * other) & 255
-            b = int(self.b * other) & 255
+            r = clamp(self.r * other)
+            g = clamp(self.g * other)
+            b = clamp(self.b * other)
         return Color(r, g, b)
 
 

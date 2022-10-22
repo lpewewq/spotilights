fb61094bcebfe14da01b98eeb40ee2cf68d42c5c
Lars Fischer <lars.fischer@rwth-aachen.de>
2022-10-22 19:24:00 +0200

    strobe and loudness scaling for philipps

--- api/animation/complex/philipp.py
+++ api/animation/complex/philipp.py
@@ -1,12 +1,12 @@
-from ..base import Mirror, TransitionOnSection
+from ..base import Mirror, ScaleLoudness, SingleSub, StrobeOnLoudnessGradient, TransitionOnSection
 from ..basic import Animation1, Animation2, Animation3, Animation4
 
 
-class PhilippsAnimation(TransitionOnSection):
+class PhilippsAnimation(SingleSub):
     def __init__(self) -> None:
         animation1 = Mirror(Animation1(), 2, [False, True])
         animation2 = Mirror(Animation2(), 2, [False, True])
         animation3 = Animation3()
         animation4 = Mirror(Animation4(), 2, [False, True])
         animations = [animation1, animation2, animation3, animation4]
-        super().__init__(animations=animations, start=0)
+        super().__init__(StrobeOnLoudnessGradient(ScaleLoudness(TransitionOnSection(animations))))

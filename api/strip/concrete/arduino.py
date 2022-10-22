f85b372ef1224e853100cd9bf0f1a84415efdf29
Lars Fischer <lars.fischer@rwth-aachen.de>
2022-10-22 14:02:19 +0200

    arduino com port fallback

--- api/strip/concrete/arduino.py
+++ api/strip/concrete/arduino.py
@@ -1,5 +1,7 @@
 import numpy as np
 from serial import EIGHTBITS, PARITY_NONE, STOPBITS_ONE, Serial
+from serial.serialutil import SerialException
+from serial.tools import list_ports
 
 from ..abstract import AbstractStrip
 
@@ -17,14 +19,25 @@ class ArduinoStrip(AbstractStrip):
         super().__init__(num_pixels, xy)
         self.header = bytes(header)
         self.brightness = brightness
-        self.serial_connection = Serial(
-            port=port,
-            baudrate=baudrate,
-            bytesize=EIGHTBITS,
-            parity=PARITY_NONE,
-            stopbits=STOPBITS_ONE,
-            timeout=None,
-        )
+        while True:
+            try:
+                self.serial_connection = Serial(
+                    port=port,
+                    baudrate=baudrate,
+                    bytesize=EIGHTBITS,
+                    parity=PARITY_NONE,
+                    stopbits=STOPBITS_ONE,
+                    timeout=None,
+                )
+                break
+            except SerialException as e:
+                print(e)
+                print("Choose other port:")
+                port_list = list_ports.comports()
+                for i, p in enumerate(port_list):
+                    print(f"{i}: {p}")
+                i = int(input())
+                port = port_list[i].device
         self.clear()
 
     def show(self,  colors: np.ndarray) -> None:

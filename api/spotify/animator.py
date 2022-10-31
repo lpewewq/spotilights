88bf703742224a7ffb339617cfd28185665e7825
Lars Fischer <lars.fischer@rwth-aachen.de>
2022-10-31 13:43:09 +0100

    async playback update

--- api/spotify/animator.py
+++ api/spotify/animator.py
@@ -1,7 +1,7 @@
 import asyncio
 import time
 
-from ..animation import AnimationModel
+from ..animation import Animation, AnimationModel
 from ..strip.abstract import AbstractStrip
 from .updater import SpotifyUpdater
 
@@ -11,50 +11,81 @@ class SpotifyAnimator:
         self.spotify_updater = spotify_updater
         self.strip = strip
         self.update_interval = update_interval
-        self.animation_loop: asyncio.Task = None
+        self.animation_task: asyncio.Task = None
+        self.update_task: asyncio.Task = None
+        self.animation: Animation = None
 
     def start(self, animation_model: AnimationModel) -> None:
-        self.stop()
-        self.animation_loop = asyncio.create_task(self.animate(animation_model))
+        if self.animation is None or not isinstance(self.animation, animation_model.animation):
+            self.animation = animation_model.construct()
+            animation_changed = True
+        else:
+            animation_changed = self.animation.update_config(animation_model.config)
+
+        if animation_changed and self.spotify_updater.audio_analysis is not None:
+            self.animation.on_track_change(self.spotify_updater.audio_analysis)
+
+        if self.animation_task is None:
+            self.animation_task = asyncio.create_task(self.animate())
 
     def stop(self) -> None:
-        if self.animation_loop is not None and not self.animation_loop.done():
-            self.animation_loop.cancel()
+        if self.animation_task is not None and not self.animation_task.done():
+            self.animation_task.cancel()
+            self.animation_task = None
+        self.strip.clear()
 
-    async def animate(self, animation_model: AnimationModel) -> None:
-        needs_spotify = animation_model.config.needs_spotify
-        animation = animation_model.construct()
-        callbacks = [
-            animation.on_section,
-            animation.on_bar,
-            animation.on_beat,
-            animation.on_tatum,
-            animation.on_segment,
-        ]
-
+    async def animate(self) -> None:
         loop_count = 0
         loop_start = time.time()
         next_spotify_update = loop_start
         while True:
             loop_count += 1
             now = time.time()
-            if needs_spotify:
-                progress = self.spotify_updater.progress()
-                if now > next_spotify_update or self.spotify_updater.duration_overflow(progress):
-                    track_changed = await self.spotify_updater.update()
-                    next_spotify_update = now + self.update_interval
-                    if track_changed:
-                        animation.on_track_change(self.spotify_updater.audio_analysis)
-                self.spotify_updater.trigger_callbacks(progress, callbacks)
-                colors = animation.render(progress, self.strip.xy)
-            else:
-                progress = now - loop_start
-                colors = animation.render(progress, self.strip.xy)
 
+            if self.animation.config.needs_spotify:
+                progress = self.spotify_updater.progress()
+
+                # check for playback update
+                if self.update_task is None:
+                    if now > next_spotify_update or self.spotify_updater.duration_overflow(progress):
+                        self.update_task = asyncio.create_task(self.spotify_updater.update())
+
+                # handle event callbacks
+                self.spotify_updater.trigger_callbacks(
+                    progress,
+                    [
+                        self.animation.on_section,
+                        self.animation.on_bar,
+                        self.animation.on_beat,
+                        self.animation.on_tatum,
+                        self.animation.on_segment,
+                    ],
+                )
+
+                # render animation
+                colors = self.animation.render(progress, self.strip.xy)
+            else:
+                # spotifyless animation
+                progress = now - loop_start
+                colors = self.animation.render(progress, self.strip.xy)
+
+            # draw animation to strip
             self.strip.show(colors)
+
+            # prevent blocking
             await asyncio.sleep(0)
 
-            if loop_count % 10 == 0:
+            # handle pending update task
+            if self.update_task is not None and self.update_task.done():
+                next_spotify_update = now + self.update_interval
+                track_changed = self.update_task.result()
+                self.update_task = None
+                # handle track changed event
+                if track_changed and self.animation.config.needs_spotify:
+                    self.animation.on_track_change(self.spotify_updater.audio_analysis)
+
+            # stats
+            if loop_count % 30 == 0:
                 now = time.time()
-                print(f"FPS: {10 / (now - loop_start):.02f}, Progress: {progress:.02f}s".ljust(50), end="\r")
+                print(f"Progress: {progress:.02f}s, {30 / (now - loop_start):.02f} FPS".ljust(50), end="\r")
                 loop_start = now
